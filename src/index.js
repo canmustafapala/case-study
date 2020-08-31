cc04edd9df7637ad066b20c7d324d81a4785fb26
Can Mustafa PALA <canmustafapala@gmail.com>
2020-08-31 23:31:48 +0300

    Env File

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const server = require('./server');
 
-const port = process.env.APP_PORT || 3000;
+const port = process.env.APP_PORT || 5000;
 
 server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
